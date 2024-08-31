7d4398f822eb4ed1df7b0117121afde83e164058
Pavel Sobolev <contact@paveloom.dev>
2024-08-31 18:54:20 +0300

    Remove `flake-utils` from the flake inputs.

--- flake.nix
+++ flake.nix
@@ -1,41 +1,41 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        name = "gnome-shell-memento-mori-shell";
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "gnome-shell-memento-mori-shell";
 
-        nativeBuildInputs = with pkgs; [
-          alejandra
-          bashInteractive
-          nil
+      nativeBuildInputs = with pkgs; [
+        alejandra
+        bashInteractive
+        nil
 
-          gettext
-          gnome.gnome-shell
-          gobject-introspection
-          libxml2
+        gettext
+        gnome.gnome-shell
+        gobject-introspection
+        libxml2
 
-          nodejs_latest
-          nodePackages_latest.npm-check-updates
-        ];
+        nodejs_latest
+        nodePackages_latest.npm-check-updates
+      ];
 
-        env = {
-          GIR_DIRECTORIES = nixpkgs.lib.concatStringsSep " " (with pkgs; [
-            "${gnome.mutter}/lib/mutter-13"
-            "${gnome.gnome-shell}/share/gnome-shell"
-            "${gobject-introspection.dev}/share/gir-1.0"
-            "${gtk4.dev}/share/gir-1.0"
-            "${libadwaita.dev}/share/gir-1.0"
-          ]);
-        };
+      env = {
+        GIR_DIRECTORIES = nixpkgs.lib.concatStringsSep " " (with pkgs; [
+          "${gnome.mutter}/lib/mutter-13"
+          "${gnome.gnome-shell}/share/gnome-shell"
+          "${gobject-introspection.dev}/share/gir-1.0"
+          "${gtk4.dev}/share/gir-1.0"
+          "${libadwaita.dev}/share/gir-1.0"
+        ]);
       };
-    });
+    };
+  };
 }
